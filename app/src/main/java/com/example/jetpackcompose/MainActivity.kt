d812fca52ebc65816a57e15d31ad38931ca14d38
vijay-nailwal <vijay.nailwal@outlook.com>
2025-03-06 11:50:12 +0530

    1.rememberSaveable

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,14 +34,17 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun MainScreen() {
-        var counter = remember { mutableStateOf(0) }
+        //orientation state not save
+        //var counter by remember { mutableStateOf(0) }
+        //orientation state save
+        var counter by rememberSaveable { mutableStateOf(0) }
         var incrementCounter = {
-            Log.d(TAG, "MainScreen: incrementCounter is ${counter.value}")
-            counter.value = counter.value + 1
+            Log.d(TAG, "MainScreen: incrementCounter is ${counter}")
+            counter = counter + 1
         }
         var decrementCounter = {
-            Log.d(TAG, "MainScreen: decrementCounter is ${counter.value}")
-            counter.value = counter.value - 1
+            Log.d(TAG, "MainScreen: decrementCounter is ${counter}")
+            counter = counter - 1
         }
 
         Column(
@@ -52,17 +58,15 @@ class MainActivity : ComponentActivity() {
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 Button(
-                    onClick = { decrementCounter.invoke() },
-                    modifier = Modifier.padding(5.dp)
+                    onClick = { decrementCounter.invoke() }, modifier = Modifier.padding(5.dp)
                 ) {
                     Text(text = "Decrement")
                 }
 
-                Text(text = "${counter.value} ")
+                Text(text = "${counter} ")
 
                 Button(
-                    onClick = { incrementCounter.invoke() },
-                    modifier = Modifier.padding(5.dp)
+                    onClick = { incrementCounter.invoke() }, modifier = Modifier.padding(5.dp)
                 ) {
                     Text(text = "Increment")
                 }
