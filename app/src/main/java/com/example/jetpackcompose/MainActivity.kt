ddc91ea83b1d111aa9e82345cd9defc6917a7a7a
vijay-nailwal <vijay.nailwal@outlook.com>
2025-03-07 13:57:35 +0530

    modifiers(sequnece matter)

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,50 +36,36 @@ import com.example.jetpackcompose.ui.theme.CountryInfoAppTheme
 class MainActivity : ComponentActivity() {
 
     private val TAG = "MainActivity"
-    private val countryList = getCountryList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen(countryList = countryList)
+            MainScreen("vijay")
         }
     }
 }
 
 @Composable
-fun MainScreen(countryList: List<CountryInfo>) {
-    CountryInfoAppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.surface
-        ) {
-            LazyColumn {
-                items(countryList) {
-                    CountryCard(countryInfo = it)
-                }
-            }
-        }
-    }
-}
+fun MainScreen(name: String, modifier: Modifier = Modifier) {
 
-@Composable
-fun CountryCard(countryInfo: CountryInfo) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth(1.0f)
-            .padding(5.dp)
-            .border(1.dp, Color.LightGray)
-            .wrapContentHeight(align = Alignment.Top),
-        shadowElevation = 2.dp
-
+    //in modifier sequence important
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        CountryCardWithConstraintLayout(countryInfo = countryInfo)
+        Text(
+            text = "hello $name",
+            modifier = Modifier.width(200.dp).height(100.dp).border(1.dp, Color.Blue)
+                .background(Color.LightGray)
+                .clickable { Log.d("MainActivity", "MainScreen: clicked") }.padding(10.dp)
+                .clip(RoundedCornerShape(5.dp))
+        )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MainScreen(getCountryList())
+    MainScreen("vijay")
 }
